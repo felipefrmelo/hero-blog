bd53db9c4f75637c83970821c5ec6e1a371dacd7
felipe <felipefrmelo@gmail.com>
2021-11-16 08:31:44 -0300

    fix

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -3,6 +3,6 @@ import { default as NextImage } from "next/image";
 import { baseUrl } from "../services/api";
 
 export const Image = ({ src, alt, ...props }: ImageProps) => {
-  console.log(`${baseUrl}${src}`);
-  return <NextImage src={`${process.env.BASE_URL}${src}`} alt={alt} {...props} />;
+  
+  return <NextImage src={`${baseUrl}${src}`} alt={alt} {...props} />;
 };
